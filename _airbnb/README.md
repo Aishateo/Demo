590fb84b5dbdf99a80c3e3c011c6bee013e7d51f
Sea <sea01mabulana@gmail.com>
2023-10-15 13:53:55 +0200

    AirBnB

---
+++ _airbnb/README.md
@@ -0,0 +1,1 @@
+# HBNB Console Documentation
---
+++ _airbnb/models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module contains an instance of FileStorage"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
